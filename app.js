6c248efaf9d4410dd858693b47253a4cfd83cb20
jianxin.zhu <zjx1247225@163.com>
2016-06-01 14:55:42 +0800

    添加一些中间件

--- app.js
+++ app.js
@@ -5,10 +5,24 @@ let path    = require('path');
 let app     = express();
 let router  = require('./router.js');
 
+let bodyParser = require('body-parser');
+let multer = require('multer');
+let session = require('express-session');
+
+
+
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/test');
+let Schema = mongoose.Schema
+
 
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'html');
 //app.engine('.html', ejs.__express);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
 
 app.use(router);
 
